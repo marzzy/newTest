88a39f7b572aacbf565cf890726cbe86505f8587
marzzy mogh <marzzy.m@gmail.com>
2019-06-11 23:26:05 +0430

    remove code spliting and add button to post which update query params
     Changes to be committed:
    	modified:   newtest/src/Routers.js
    	modified:   newtest/src/components/posts/Posts.js

--- newtest/src/Routers.js
+++ newtest/src/Routers.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import asyncComponent from './components/AsyncComponent';
+import Home from "./components/home/Home";
+import Posts from "./components/posts/Posts";
+import NotFound from "./components/notFound/NotFound";
+// import asyncComponent from './components/AsyncComponent';
 
 /* Use components to define routes */
-const AsyncHome = asyncComponent(() => import("./components/home/Home"));
-const AsyncPost = asyncComponent(() => import("./components/posts/Posts"));
-const AsyncNotFound = asyncComponent(() => import("./components/notFound/NotFound"));
 
 export default () => 
   <Router>
@@ -25,9 +25,9 @@ export default () =>
         </li>
       </ul>
       <Switch>
-        <Route path="/" exact component={AsyncHome} />
-        <Route path="/posts/:id" exact component={AsyncPost} />
-        <Route component={AsyncNotFound} />
+        <Route path="/" exact component={Home} />
+        <Route path="/posts/:id" exact component={Posts} />
+        <Route component={NotFound} />
       </Switch>
     </div>
   </Router>;
--- newtest/src/components/posts/Posts.js
+++ newtest/src/components/posts/Posts.js
@@ -1,11 +1,43 @@
 import React from 'react';
 
-function Posts() {
-  return (
-    <div>
-      posts
-    </div>
-  );
+class Posts extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  componentDidMount() {
+    let queryList = this.getParams(this.props.location);
+    console.log('this.props', this.props);
+    console.log('queryList', queryList);
+  }
+  
+  handleClick() {
+    const newUrl = this.setParams({ query: "javascript" });
+    console.log('newUrl', newUrl);
+    this.props.history.push(`?${newUrl}`)
+  }
+  getParams(location) {
+    console.log('hi');
+    const searchParams = new URLSearchParams(location.search);
+    return {
+      query: searchParams.get('query') || '',
+    };
+  }
+
+  setParams({query = ''}) {
+    const searchParams = new URLSearchParams();
+    searchParams.set('query', query);
+    return searchParams.toString();
+  }
+
+  render () {
+    return (
+      <div>
+        posts
+        <button onClick={this.handleClick} >click to search</button>
+      </div>
+    );
+  }
 }
 
 export default Posts;
