2ca865fcc349f11223457c7940af974bed7165aa
marzzy mogh <marzzy.m@gmail.com>
2019-06-14 22:06:14 +0430

    add react-loadable and use it and a bit change on react-router
     Changes to be committed:
    	modified:   package.json
    	modified:   src/Routers.js
    	modified:   src/index.css
    	deleted:    src/newFile.js

--- newtest/src/Routers.js
+++ newtest/src/Routers.js
@@ -1,32 +1,78 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import Home from "./components/home/Home";
-import Posts from "./components/posts/Posts";
-import NotFound from "./components/notFound/NotFound";
-// import asyncComponent from './components/AsyncComponent';
+import loadable from 'react-loadable';
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 
-/* Use components to define routes */
+function Loading(props) {
+  if (props.error){
+    // When the loader has errored
+    return 'oooppppsssss';
+
+  } else if (props.pastDelay) {
+    // When the loader has taken longer than the delay
+    return (<h3>Loading ... we know thats take a bit long,but plz keep calm ...</h3>);
+
+  } else if (props.timedOut) {
+    // When the loader has taken longer than the timeout
+    return (
+      <div>
+        Taking a long time... 
+        <button onClick={props.retry}>Retry</button>
+      </div>);
+
+  } else {
+    // When the loader has just started
+    return null;
+
+  }
+}
+
+const Home = loadable ({
+  loader: () => import('./components/home/Home'),
+  loading: Loading
+});
+
+// 1. delay : Time to wait(in milliseconds) before passing props.pastDelay to your
+//      loading component.This defaults to 200.
+// 2. timeout: Time to wait(in milliseconds) before passing props.timedOut to your
+//      loading component.This is turned off by default.
+// 3. render : A function to customize the rendering of loaded modules.
+        // example: it was more than 1 import in a file
+const Posts = loadable({
+  loader: () => import('./components/posts/Posts'),
+  loading: Loading,
+  delay: 300,
+  timeout: 5000,
+  // render(loaded, props) {
+  //   let Component = loaded.default;
+  //   return <Component {...props} />;
+  // } // its just an example :D 
+});
+
+const NotFound = loadable({
+  loader: () => import('./components/notFound/NotFound'),
+  loading: Loading
+});
 
 export default () => 
   <Router>
     <div>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <NavLink activeClassName="active" to="/">Home</NavLink>
         </li>
         <li>
-          <Link to="/posts/12">post</Link>
+          <NavLink activeClassName="active" to="/posts/12">post</NavLink>
         </li>
         <li>
-          <Link to="/posts/54">hiiiii</Link>
+          <NavLink activeClassName="active" to="/posts/54">hiiiii</NavLink>
         </li>
         <li>
-          <Link to="/ahmadpath">not matched</Link>
+          <NavLink activeClassName="active" to="/ahmadpath">not matched</NavLink>
         </li>
       </ul>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/posts/:id" exact component={Posts} />
+        <Route path="/posts/:id" component={Posts} />
         <Route component={NotFound} />
       </Switch>
     </div>
